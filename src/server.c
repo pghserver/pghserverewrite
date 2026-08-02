617d199673a34045e16b6e3bffc7077ad5271351
boyninja1555 <flappyfloorg@gmail.com>
2026-08-02 16:50:27 -0500

    Basic HTTP request logging with picohttpparser

--- src/server.c
+++ src/server.c
@@ -1,9 +1,11 @@
 #include "server.h"
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <picohttpparser.h>
 
 int pgh_listen(const PghArgs args)
 {
@@ -17,6 +19,37 @@ int pgh_listen(const PghArgs args)
 void *pgh_handle(void *arg)
 {
     int client = (intptr_t)arg;
+
+    char buffer[8192];
+    ssize_t length = read(client, buffer, sizeof(buffer) - 1);
+    if (length <= 0)
+    {
+        close(client);
+        return NULL;
+    }
+
+    buffer[length] = '\0';
+
+    const char *method;
+    size_t method_length;
+
+    const char *path;
+    size_t path_length;
+
+    int minor_version;
+
+    struct phr_header headers[32];
+    size_t num_headers = 32;
+
+    int result = phr_parse_request(buffer, length, &method, &method_length, &path, &path_length, &minor_version, headers, &num_headers, 0);
+    if (result < 0)
+    {
+        fprintf(stderr, "Invalid HTTP request!\n");
+        close(client);
+        return NULL;
+    }
+
+    printf("%.*s %.*s HTTP/1.%d\n", (int)method_length, method, (int)path_length, path, minor_version);
     close(client);
     return NULL;
 }
